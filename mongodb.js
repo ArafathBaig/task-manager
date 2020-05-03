0c720b199143438d23817f9f216a5ca77a600afb
Arafat Najjashi <arafath.moghul@gmail.com>
2020-05-03 16:35:34 +0530

    read data

--- mongodb.js
+++ mongodb.js
@@ -8,10 +8,6 @@ const {MongoClient, ObjectId} = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectId()
-console.log(id);
-console.log(id.getTimestamp())
-
 MongoClient.connect(connectionURL,{ useNewUrlParser : true }, (error, client) => {
 
     if(error) {
@@ -19,54 +15,13 @@ MongoClient.connect(connectionURL,{ useNewUrlParser : true }, (error, client) =>
     }
 
     const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        _id: id,
-        name: 'Vikram',
-        age: 26
-    }, (error, result) => {
-        if(error){
-            return console.log("Unable to insert!")
-        }
+    
+    // db.collection('users').find({ name : 'Arafath'}).toArray((error, users)  => {
+    //     console.log(users)
+    // })
 
-        console.log(result.ops)
+    db.collection('tasks').find({ completed : false}).toArray((error, users) => {
+        console.log(users)
     })
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Arafath',
-    //         age: 20
-    //     },
-    //     {
-    //         name:'Najjashi',
-    //         age: 22
-    //     }
-    // ], (error,result) => {
-
-    //     if(error){
-    //         return console.log("Unable to Insert");
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: "Do it.",
-    //         completed: true
-    //     },
-    //     {
-    //         description: "Ate food.",
-    //         completed: false
-    //     },
-    //     {
-    //         description: "Champestanu",
-    //         completed: true
-    //     }
-    // ], (error, result) => {
-    //     if(error){
-    //         return console.log("Unable to insert")
-    //     }
-
-    //     console.log(result.ops)
-    // })
+       
 })
