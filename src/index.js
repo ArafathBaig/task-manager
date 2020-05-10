cbea281d7096d0a2c5686ee35f7f4f4236353c6c
Arafat Najjashi <arafath.moghul@gmail.com>
2020-05-10 23:58:30 +0530

    accepting authentication tokens

--- src/index.js
+++ src/index.js
@@ -8,17 +8,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if(req.method === 'GET'){
-//         res.send('GET requests are disabled')
-//     }else{
-//         next()
-//     }
-// })
-
-app.use((req, res, next) => {
-    res.status(503).send('We will send back info shortly')
-})
 
 app.use(express.json())
 app.use(userRouter)
