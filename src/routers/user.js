b3b2b25c8bf61a439496dbff6efdb52e827a07ca
Arafat Najjashi <arafath.moghul@gmail.com>
2020-05-21 20:18:57 +0530

    jest

--- src/routers/user.js
+++ src/routers/user.js
@@ -13,7 +13,7 @@ router.post('/users' , async(req, res) => {
 
     try{
         await user.save()
-        sendWelcomeEmail(user.email, user.name)
+        //sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({user , token})
     }catch(e){
@@ -94,7 +94,7 @@ router.delete('/users/me' , auth,  async (req,res) => {
     try{
         
         await req.user.remove()
-        sendCancellationEmail(req.user.email, req.user.name)
+        //sendCancellationEmail(req.user.email, req.user.name)
         res.send(req.user)
     }catch(e){
         res.status(500).send()
