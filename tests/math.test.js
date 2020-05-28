bcf047ca4d7c05f2a63f0833a907e11cb3db2347
Arafat Najjashi <arafath.moghul@gmail.com>
2020-05-28 17:06:09 +0530

    async jest testing

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip } = require('../src/math')
+const { add, calculateTip,fareheitToCelsius, celsiusToFarenheit } = require('../src/math')
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3)
@@ -12,4 +12,23 @@ test('Should calculate total with tip', () => {
 test('should calculate total with default tip', () => {
     const total = calculateTip(10)
     expect(total).toBe(12.5)
+})
+
+test('Should convert 32 F to 0 C', () => {
+    const f= fareheitToCelsius(32)
+    expect(f).toBe(0)
+})
+
+
+test('Should add two numbers', (done) => {
+    add(2,3).then((sum) => {
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+
+test('Should add two numbers async/await', async () => {
+    const sum = await add(10,22)
+    expect(sum).toBe(32)
 })
