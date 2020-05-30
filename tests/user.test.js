cb71135dc2c7b32b09eb902dd93b21c4dd3702f8
Arafat Najjashi <arafath.moghul@gmail.com>
2020-05-30 16:22:57 +0530

    Creating, login test done

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,18 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Arafath',
+    email:'whyareyousoougly@gmail.com',
+    password: 'Classical99'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
 
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
@@ -7,4 +20,18 @@ test('Should signup a new user', async () => {
         email:'arafa@gmailllll.com',
         password: 'Arraba123'
     }).expect(201)
+})
+
+test('Should log in existing user', async () => {
+    await request(app).post('/users/login').send({
+        email:userOne.email,
+        password:userOne.password
+    }).expect(200)
+})
+
+test('Should not login non exitent user', async() => {
+    await request(app).post('/users/login').send({
+        email: 'osfh@gmail.com',
+        password: 'hssjdf'
+    }).expect(400)
 })
