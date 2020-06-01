36851954d7ab8b2136f4b9d9c0b2d459a3b21161
Arafat Najjashi <arafath.moghul@gmail.com>
2020-06-01 17:10:36 +0530

    wrapping up user tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -93,3 +93,38 @@ test('Should not delete account', async () => {
     .send()
     .expect(401)
 })
+
+test('Should upload avatar image', async() => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200)
+
+        const user = await User.findById(userOneId)
+        expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async ()=> {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: 'Jess'
+        })
+        .expect(200)
+
+        const user = await User.findById(userOneId)
+        expect(user.name).toEqual('Jess')
+})
+
+test('Should not update invvalid user fields', async ()=> {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location: 'Jess'
+        })
+        .expect(400)
+
+})
